d79906b38ca4b0895dfc17d9c0a9dde364ca570d
ColinChan520 <Colinchan233@gmail.com>
2024-08-31 14:40:36 +0930

    chore: Refactor App component and add Header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-black text-white">
+      <div className="container mx-auto flex justify-between items-center py-4">
+        {/* Logo Section */}
+        <div className="text-xl font-bold">
+          <img src="/src/assets/atsys-logo.png" alt="ATSYS" className="h-13" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
