707e52b92a0a3907d83899ebcf90da1f4d8cad5f
Xiao Liu <a1878510@adelaide.edu.au>
2024-09-30 02:55:35 +0930

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import Header from './components/Header';
 import DragAndDrop from './components/DragAndDrop';
 import QueryDisplay from './components/QueryDisplay';
-import GrafanaIframe from './components/GrafanaIframe';
 import InfluxDBAPI from './components/InfluxDBAPI';
 
 function App() {
