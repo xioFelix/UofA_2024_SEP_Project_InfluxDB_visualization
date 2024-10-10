615d0efcb222861cda2a6b6fcf48f277531b97d8
Xiao Liu <a1878510@adelaide.edu.au>
2024-10-10 13:22:46 +1030

    Create index.tsx

---
+++ frontend/src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline /> {}
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+);
