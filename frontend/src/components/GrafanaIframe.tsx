1171cd6a7f085b59ce6a67b2c5cb21bf0bc2d038
Xiao Liu <a1878510@adelaide.edu.au>
2024-10-10 13:22:38 +1030

    Update GrafanaIframe.tsx

--- frontend/src/components/GrafanaIframe.tsx
+++ frontend/src/components/GrafanaIframe.tsx
@@ -9,13 +9,23 @@ const GrafanaIframe: React.FC<GrafanaIframeProps> = ({ dashboardUrl }) => {
     return (
         <>
             {dashboardUrl ? (
-                // Display the Grafana dashboard using the provided URL inside an iframe
-                <iframe
-                    src={`http://localhost:3000${dashboardUrl}?orgId=1&refresh=1s&viewPanel=1`}
-                    width="100%"
-                    height="600"
-                    frameBorder="0"
-                ></iframe>
+                <Box sx={{ marginTop: 4, textAlign: 'center' }}>
+                    {/* Title */}
+                    <Typography variant="h6" align="center">
+                        Grafana Dashboard
+                    </Typography>
+                    {/* Embedded Grafana iframe */}
+                    <iframe
+                        src={`http://localhost:3000${dashboardUrl}?orgId=1&refresh=1s&viewPanel=1`}
+                        width="100%"
+                        height="600"
+                        frameBorder="0"
+                        style={{
+                            border: '1px solid #ccc',
+                            borderRadius: '8px',
+                        }}
+                    ></iframe>
+                </Box>
             ) : (
                 <p>No dashboard to display</p> // Message when no dashboard URL is set
             )}
