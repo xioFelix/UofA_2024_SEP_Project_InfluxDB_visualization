fc0c76b5223d08360797f025dfdf728ecf75bb3a
Xiao Liu <a1878510@adelaide.edu.au>
2024-10-10 13:22:08 +1030

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -52,21 +52,8 @@ function App() {
         <QueryDisplay />
       </Box>
 
-      {/* Display the Grafana dashboard in an iframe if a dashboard URL is set */}
-      <div className="mt-16">
-        <h2>Grafana Dashboard</h2>
-        {dashboardUrl ? (
-          // Display the Grafana dashboard using the provided URL inside an iframe
-          <iframe
-            src={`http://localhost:3000${dashboardUrl}?orgId=1&refresh=1s&viewPanel=1`}
-            width="100%"
-            height="600"
-            frameBorder="0">
-          </iframe>
-        ) : (
-          <p>No dashboard to display</p> // Message when no dashboard URL is set
-        )}
-      </div>
+      {/* Render the GrafanaIframe component, passing the dashboardUrl */}
+      <GrafanaIframe dashboardUrl={dashboardUrl} />
 
       {/* Render the InfluxDBAPI component to display any InfluxDB data
       <div className="mt-16">
